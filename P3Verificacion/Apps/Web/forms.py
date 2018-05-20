4f8b34b0f0cbb13a9e113f7b59dbe04f8b15007b
Enrique Sánchez <enrique.sanchez@cabify.com>
2018-05-20 17:28:23 +0200

    Corrections

--- P3Verificacion/Apps/Web/forms.py
+++ P3Verificacion/Apps/Web/forms.py
@@ -1,4 +1,5 @@
 from django import forms
 
+
 class formulario(forms.Form):
-    mensaje = forms.CharField(widget=forms.Textarea, label='Introduce el texto (100 caracteres max.):', max_length = 100)
+    mensaje = forms.CharField(widget=forms.Textarea, label='Introduce el texto (100 caracteres max.):', max_length=100)
---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+setup(
+    name='Practica 3 BDD'
+)
